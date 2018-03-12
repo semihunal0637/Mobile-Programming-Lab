df7aa0e04b10504c3cd652fc5787b488662236be
Semih Ünal <semih1837@gmail.com>
2018-03-12 21:05:31 +0300

    12.03.2018  -- 9:05pm

---
+++ LabUygulama/app/src/main/java/com/example/semih/labuygulama/Main2Activity.java
@@ -0,0 +1,64 @@
+package com.example.semih.labuygulama;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+
+            }
+        });
+        textView = (TextView)findViewById(R.id.textView);
+
+        Bundle alinanMesaj = getIntent().getExtras();
+        String deger = alinanMesaj.getString("bilgi");
+
+        if(deger!= null)
+        {
+            Toast.makeText(getApplicationContext(), "Bilgi Alındı", Toast.LENGTH_SHORT).show();
+            textView.setText("Gelen Bilgi : " + deger);
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Hata", Toast.LENGTH_SHORT).show();
+
+        }
+
+        ImageView resim = (ImageView)findViewById(R.id.imageView);
+        resim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://www.selcuk.edu.tr";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
---
+++ LabUygulama/app/src/main/java/com/example/semih/labuygulama/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.semih.labuygulama;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button buton;
+    Button GecisButon;
+    EditText mesajText;
+    String durum="tek";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final ConstraintLayout constraintLayout = (ConstraintLayout)findViewById(R.id.Clayout);
+
+
+        textView = (TextView) findViewById(R.id.text1);
+        buton = (Button)findViewById(R.id.button);
+        buton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(durum=="tek")
+                {
+                    durum="cift";
+                    textView.setText("Şöyle");
+                    textView.setTextColor(Color.GREEN);
+                    constraintLayout.setBackgroundResource(R.drawable.arka180);
+                }
+                else
+                {
+                    durum="tek";
+                    textView.setText("Böyle");
+                    textView.setTextColor(Color.YELLOW);
+                    constraintLayout.setBackgroundResource(R.drawable.arka);
+                }
+            }
+        });
+
+        GecisButon = (Button) findViewById(R.id.button2);
+        GecisButon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mesajText = (EditText)findViewById(R.id.editText);
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("bilgi",mesajText.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+
+}
